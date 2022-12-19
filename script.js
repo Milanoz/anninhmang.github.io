f3d7bdef7c4fef712db3219512958014596df7b7
Nguyễn Văn Huy <104371651+Milanoz@users.noreply.github.com>
2022-12-19 22:17:05 +0700

    Add files via upload
    Signed-off-by: Nguyễn Văn Huy <104371651+Milanoz@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,471 @@
+var questions =[
+    {
+        num : 1,
+        question : "Ứng dụng mạng nào có thể được sử dụng để phân tích và kiểm tra lưu lượng mạng ?",
+        answer: "Sniffer",
+        options : [
+            "Sniffer",
+            "Router",
+            "FTP",
+            "IDS"
+        ]
+    },
+    {
+        num: 2,
+        question: "Keberos là dịch vụ ủy thác",
+        answer: "Xác thực trên Server",
+        options: [
+            "Xác thực trên Server",
+            "Xác thực trên Web",
+            "Xác thực X.59",
+            "Xác thực trên các máy trạm với nhau"
+        ]
+    },
+    {
+    num: 3,
+    question: "PGP là giao thức để xác thực",
+    answer: "Bảo mật cho thư điện tử",
+    options: [
+            "Bảo mật cho thư điện tử",
+            "Quyền đăng cập vào hệ thống máy chủ Window",
+            "Thực hiện mã hóa thông điệp theo thuật tóan RSA",
+            "Địa chỉ của máy trạm khi kết nối vào Internet"
+        ]
+    },
+    {
+    num: 4,
+    question: "Công cụ/cơ chế  bảo mật cho mạng không dây là",
+    answer: "WEP",
+    options: [
+        "	 	SSL	 	",
+        "	 	TSL	 	",
+        "	 	WEP	 	",
+            "Giao thức PGP"
+        ]
+    },
+    {
+    num: 5,
+    question: "Giao thứ SSL và TSL hoạt động ở tầng nào của mô hình OSI",
+    answer: "Transport",
+    options: [
+        "Network",
+        "Transport",
+            "Sesion",
+            "Từ tầng Trasport trở lên"
+        ]
+    },
+    {
+    num: 6,
+    question: "Chức năng chính của Virus là",
+    answer: "Sống ký sinh và lây nhiễm",
+    options: [
+        "Lây nhiễm và sinh sản",
+        "Tự phát triển độc lập và lây nhiễm",
+        "Tất cả đều đúng",
+        "Sống ký sinh và lây nhiễm"
+        ]
+    },
+    {
+    num: 7,
+    question: "Giao thức SSL dùng để",
+    answer: "Cung cấp bảo mật cho dữ liệu lưu thông trên dịch vụ HTTP",
+    options: [
+        "Cung cấp bảo mật cho thư điện tử",
+        "Cung cấp bảo mật cho dữ liệu lưu thông trên dịch vụ HTTP",
+        "Cung cấp bảo mật cho Web",
+        "Cung cấp bảo mật cho xác thực người dùng vào các hệ thống vận hành trên Flatform Window"
+        ]
+    },
+    {
+    num: 8,
+    question: "Các giao thức bảo mật trên Internet như SSL, TLS và SSH hoạt động ở tầng nào trên mô hình OSI",
+    answer: "Từ tầng Transport trở lên đến tầng 7",
+    options: [
+        "Tầng Network",
+        "Từ tầng Transport trở lên đến tầng 7",
+            "Tầng Transport",
+            "Tầng Session"
+        ]
+    },
+    {
+    num: 9,
+    question: "Kỹ thuật tấn công phổ biến trên Web là",
+    answer: "Từ chối dịch vụ (DoS)",
+    options: [
+        "Chiếm hữu phiên làm việ",
+        "Tràn bộ đệm.",
+        "Từ chối dịch vụ (DoS)",
+            "Chèn câu truy vấn SQL."
+        ]
+    },
+    {
+    num: 10,
+    question: "Các lỗ hổng bảo mật trên hệ thống là do",
+    answer: "Tất cả đều đúng",
+    options: [
+        "Dịch vụ cung cấp",
+        "Bản thân hệ điều hành",
+        "Con người tạo ra",
+        "Tất cả đều đúng"
+        ]
+    },
+    {
+    num: 11,
+    question: "Cho biết câu nào đúng trong các câu sau",
+    answer: "Tất cả đều đúng",
+    options: [
+            "Tất cả đều đúng",
+            "Tất cả Firewall đều có chung thuộc tính là cho phép phân biệt hay đối xử khả năng từ chối hay truy nhập dựa vào địa chỉ nguồn",
+            "Chức năng chính của Firewall là kiểm sóat luồng thông tin giữa mạng cần bảo vệ và Internet thông qua các chính sách truy nhập đã được thiết lập",
+            "Hệ thống Firewall thường bao gồm cả phần cứng lẫn phần mềm"
+        ]
+    },
+    {
+    num: 12,
+    question: "Loại Firewall nào sau đây cho phép hoạt động ở lớp phiên ( session) của mô hình OSI",
+    answer: "Circuit level firewall",
+    options: [
+            "Circuit level firewall",
+            "Packet filtering firewall",
+            "Application level firewall",
+            "Stateful multilayer inspection firewall"
+        ]
+    },
+    {
+    num: 13,
+    question: "Khi thuê một giải pháp VPN, những loại tấn công nào bạn cần phải xét đến ?",
+    answer: "Denial of Service (DoS) attacks, Internet Viruses..",
+    options: [
+            "Denial of Service (DoS) attacks, Internet Viruses..",
+            "Distributed Denial of Service (DDoS) attacks.",
+            "Data confidentiality, IP Spoofing.",
+            "Network mapping, Internet Viruses."
+        ]
+    },
+    {
+    num: 14,
+    question: "Các phát biểu sau đây phát biểu là là đúng nhất",
+    answer: "Là một điểm chặn của trong quá trình điều khiển và giám sát.",
+    options: [
+            "Là một điểm chặn của trong quá trình điều khiển và giám sát.",
+            "Firewall là một vành đai phòng thủ cho máy tính hoặc hệ thống trước những tấn công",
+            "Là một phần mềm hoặc phần ứng có khả năng ngăn chặn tấn công từ bên trong và bên ngoài vào hệ thống.",
+            "Là một giải pháp giúp hệ thống phát hiện và ngăn chặn các truy cập trái phép"
+        ]
+    },
+    {
+    num: 15,
+    question: "Bảo mật thư điện tử là nhằm đảm bảo",
+    answer: "Tính tin cẩn (confidentiality), Tính xác nhận, Toàn vẹn thông điệp (integrity), Sự thối thác ban đầu (non-repudiation of origin)",
+    options: [
+            "Tính tin cẩn (confidentiality), Tính xác nhận, Toàn vẹn thông điệp (integrity), Sự thối thác ban đầu (non-repudiation of origin)",
+            "Tính xác nhận,Toàn vẹn thông điệp(integrity), Sự thối thác ban đầu (non-repudiation of origin), tính bền vững",
+            "Sự thối thác ban đầu (non-repudiation of origin), tính bền vững, tính ổn khi gởi và nhận",
+            "Tất cả đều đúng"
+        ]
+    },
+    {
+        num : 16,
+        question : "Chữ ký điện tử (digital signature) sử dụng thuật tóan nào sau đây",
+        answer: "MD5, SHA,RSA",
+        options : [
+            "MD5, SHA,RSA",
+            "RSA,MD5",
+            "RSA,MD5, Keboros",
+            "Không dùng thuật tóan nào nêu trên"
+        ]
+    },
+    {
+        num: 17,
+        question: "Chữ ký điện tử là",
+        answer: "Là một chuỗi đã được mã hóa theo thuật tóan băm và đính kèm với văn bản gốc trước khi gởi.",
+        options: [
+            "Là một chuỗi đã được mã hóa theo thuật tóan băm và đính kèm với văn bản gốc trước khi gởi.",
+            "Đoạn dữ liệu ngắn đính kèm với văn bản gốc để chứng thực tác giả của văn bản và giúp người nhận kiểm tra tính toàn vẹn của nội dung văn bản gố",
+            "Tất cả cả đều đúng",
+        ]
+    },
+    {
+    num: 18,
+    question: "Các bước mã hóa của chứ ký điện tử",
+    answer: "Dùng giải thuật băm để thay đổi thông điệp cần truyền đi, Sử dụng khóa private key của người gửi để mã hóa message digest thu được ở bước trên, sau đó gộp digital signature vào message ban đầu",
+    options: [
+            "Dùng giải thuật băm để thay đổi thông điệp cần truyền đi, Sử dụng khóa private key của người gửi để mã hóa message digest thu được ở bước trên, sau đó gộp digital signature vào message ban đầu",
+            "Dùng giải thuật băm để thay đổi thông điệp cần truyền đi, sử dụng khóa private key của người gửi để mã hóa message digest thu được ở bước trên, sau đó gộp digital signature vào message ban đầu và nén dữ liệu gởi đi.",
+            "Chỉ sử dụng  giải thuật băm để thay đổi thông điệp cần truyền đi và sử dụng khóa private key của người gửi để mã hóa message digest thu được ở bước trên.",
+            "Tất cả đều đúng"
+        ]
+    },
+    {
+    num: 19,
+    question: "Các bước kiểm tra của chứ ký điện tử",
+    answer: "Gồm các bước 1. Dùng public key của người gửi (khóa này được thông báo đến mọi người) để giải mã chữ ký số của message, 2.Dùng giải thuật (MD5 hoặc SHA) băm message đính kèm, So sánh kết quả thu được ở bước 1 và 2. 3.Nếu trùng nhau, ta kết luận message này không bị thay đổi trong quá trình truyền và message này là của người gửi.",
+    options: [
+        "Chỉ có bước 1 và 2",
+        "Gồm các bước 1.Dùng public key của người gửi (khóa này được thông báo đến mọi người) để giải mã chữ ký số của message, 2.Dùng giải thuật (MD5 hoặc SHA) băm message đính kèm, So sánh kết quả thu được ở bước 1 và 2. 3. Nén dữ liệu rồi gởi đi",
+        "Không có bước nào ở trên là đúng",
+        "Gồm các bước 1.Dùng public key của người gửi (khóa này được thông báo đến mọi người) để giải mã chữ ký số của message, 2.Dùng giải thuật (MD5 hoặc SHA) băm message đính kèm, So sánh kết quả thu được ở bước 1 và 2. 3.Nếu trùng nhau, ta kết luận message này không bị thay đổi trong quá trình truyền và message này là của người gửi."
+        ]
+    },
+    {
+    num: 20,
+    question: "Thuật tóan thực hiện trong cơ chế bảo mật IP (IP Sec) ở Window sử dụng là",
+    answer: "MD5 và SHA1",
+    options: [
+        "Kerberos và DES",
+        "MD5 và SHA1",
+        "DES hoặc 3DES (triple DES).",
+        "Tất cả đều sai"
+        ]
+    },
+    {
+    num: 21,
+    question: "42. Hai giao thức sử dụng trong IPSec (IPSec Protocol) gồm",
+    answer: "IP Authentication Header, IP Encapsulating Security Payload",
+    options: [
+        "TCP/IP, IP Encapsulating Security Payload",
+        "IP Authentication Header, TCP/IP",
+        "IP Authentication Header, IP Encapsulating Security Payload",
+        "Tất cả đều đúng"
+        ]
+    },
+    {
+    num: 22,
+    question: "Các điểm khác nhau cơ bản giữa dịch vụ X.59 và Kerberos là",
+    answer: "Tất cả đều đúng",
+    options: [
+        "Được sử dụng trong dịch vụ mail",
+        "Xác thực nhiều chiều",
+        "Dựa trên mã hóa đối xứng",
+        "Tất cả đều đúng"
+        ]
+    },
+    {
+    num: 23,
+    question: "Các chức năng cơ bản của kỹ thuật tấn công Sniffer",
+    answer: "Bắt gói tin trên đường truyền, phân tích lỗi và giải mã gói tin",
+    options: [
+        "Tự động chụp các tên người sử dụng (Username) và mật khẩu không được mã hoá, Chuyển đổi dữ liệu trên đường truyền, phân tích những lỗi đang mắc phải trên hệ thống lưu lượng của mạng.",
+        "Bắt gói tin trên đường truyền, mã hóa vag giải mã dữ liệu",
+        "Bắt gói tin trên đường truyền, phân tích lỗi và giải mã gói tin",
+        "Tất cả đều đúng"
+        ]
+    },
+    {
+    num: 24,
+    question: "Các bước tấn công của Web Server theo trình tự sau :",
+    answer: "Thăm dò, Scan, Giành quyền truy cập, Duy trì truy cập, Xóa vết",
+    options: [
+        "Thăm dò, Scan, Giành quyền truy cập, Duy trì truy cập, Xóa vết",
+        "Scan, Thăm dò, Giành quyền truy cập, Duy trì truy cập, Xóa vết",
+        "Thăm dò, Scan, Duy trì truy cập, Giành quyền truy cập, Xóa vết",
+        "Giành quyền truy cập, Duy trì truy cập, Scan, Thăm dò"
+        ]
+    },
+    {
+    num: 25,
+    question: "Hai cơ chế chính của hệ thống IDS Trigger để phát hiện khi có một kẻ xâm nhập tấn công  mạng là :",
+    answer: "Phát hiện biểu hiện không bình thường, phát hiện sử dụng không đúng",
+    options: [
+        "Phát hiện biểu hiện không bình thường, phát hiện sử dụng không đúng",
+        "Phát hiện hiện tượng trùng lặp, phát hiện không bình thường",
+        "Phát hiện thay đổi, phát hiện sử dụng bất bình thường",
+        "Tất cả đều đúng"
+        ]
+    },
+    {
+    num: 26,
+    question: "Các chức năng cơ bản của kỹ thuật tấn công Sniffer",
+    answer: "Bắt gói tin trên đường truyền, phân tích lỗi và giải mã gói tin",
+    options: [
+            "Bắt gói tin trên đường truyền, phân tích lỗi và giải mã gói tin",
+            "Tự động chụp các tên người sử dụng (Username) và mật khẩu không được mã hoá, Chuyển đổi dữ liệu trên đường truyền, phân tích những lỗi đang mắc phải trên hệ thống lưu lượng của mạng.",
+            "Bắt gói tin trên đường truyền, mã hóa vag giải mã dữ liệu",
+            "Tất cả đều đúng"
+        ]
+    },
+    {
+    num: 27,
+    question: "Mã hóa nào sau đây là một tiêu chuẩn dùng để phát triển cho việc tạo ra thông điệp an toàn?",
+    answer: "Data Encryption Standard Data Encryption Standard",
+    options: [
+            "Data Encryption Standard Data Encryption Standard",
+            "Digital Signature Standard Digital Signature Standard",
+            "Secure Hash Algorithm Secure Hash Algorithm",
+            "Data Signature Standard"
+        ]
+    },
+    {
+    num: 28,
+    question: "Nếu kẻ tấn công lấy trộm một tệp mật khẩu có chứa các mật khẩu mã hóa một chiều, Kẻ tấn công sẽ thực hiện việc tìm kiếm mật khẩu bị mã hóa theo cách thức nào?",
+    answer: "Tấn công thư mục (Dictionary attack)",
+    options: [
+            "Tấn công thư mục (Dictionary attack)",
+            "Tấn công Man-in-the-middle",
+            "Tấn công Birthday",
+            "Tấn công Denial of Service"
+        ]
+    },
+    {
+    num: 29,
+    question: "Những gì được sử dụng để tạo ra một chữ ký điện tử?",
+    answer: "Khóa công khai của người gửi",
+    options: [
+            "Khóa công khai của người gửi",
+            "Khóa bí mật của người nhận",
+            "Khóa riêng của người gửi",
+            "Khóa công khai của người nhận"
+        ]
+    },
+    {
+    num: 30,
+    question: "Phương thức nào sau đây là tốt nhất mô tả một chữ ký điện tử?",
+    answer: "Phương pháp để cho những người nhận tin nhắn chứng minh nguồn gốc và sự toàn vẹn của một tin nhắn",
+    options: [
+            "Phương pháp để cho những người nhận tin nhắn chứng minh nguồn gốc và sự toàn vẹn của một tin nhắn",
+            "Phương thức chuyển chữ ký viết tay vào lên tài liệu điện tử",
+            "Phương pháp mã hóa thông tin bí mật",
+            "Phương pháp để cung cấp một chữ ký điện tử và mã hóa"
+        ]
+    },
+];
+
+const start_btn = document.querySelector(".start_quiz");
+const quiz_box = document.querySelector(".quiz-box");
+const que_text = quiz_box.querySelector(".que_text");
+const options_box = quiz_box.querySelector(".options");
+const next_btn = document.querySelector(".next-btn");
+const total_q = document.querySelector(".quiz-footer .total_que");
+const count_que = document.querySelector(".quiz-footer .count_que");
+const result_box = document.querySelector(".result-box");
+const total_que_r = document.querySelector(".total-que span");
+const right_ans_r = document.querySelector(".right-ans span");
+const wrong_ans_r = document.querySelector(".wrong-ans span");
+const percentage = document.querySelector(".percentage span");
+
+const again_quiz = document.querySelector(".result-footer .again-quiz");
+const exit = document.querySelector(".result-footer .exit");
+
+const mark_wrong = '<i class="fa fa-times"></i>';
+const mark_check = '<i class="fa fa-check"></i>';
+
+let shuffledQuestions
+
+shuffledQuestions = questions.sort(() => Math.random() - .5)
+
+
+start_btn.onclick =()=>{
+    quiz_box.classList.remove("inactive");
+    start_btn.classList.add("inactive");
+}
+
+total_q.innerText = questions.length;
+total_que_r.innerText = questions.length;
+
+var score = 0;
+var que_index = 0;
+var right_answers = 0;
+var wrong_answers = 0;
+count_que.innerText = que_index+1;
+ShowQuestion(que_index);
+
+function ShowQuestion(q_index){
+    que_text.innerText = questions[q_index].num+". "+ questions[q_index].question;
+var option_statement = "";
+for(var i=0; i<questions[q_index].options.length; i++){
+    option_statement += `<div class="option">${questions[q_index].options[i]}</div>`;
+    var testing = document.getElementById("point");
+    testing.innerHTML = `${score} of ${questions.length} Điểm`
+}
+
+options_box.innerHTML = option_statement;
+
+var AllOptions = options_box.querySelectorAll(".option");
+
+    for(var j=0; j<AllOptions.length; j++){
+        AllOptions[j].setAttribute("onclick","UserAnswer(this)");
+    }
+    next_btn.classList.add("inactive");
+}
+
+
+next_btn.onclick=()=>{
+    que_index++;
+    
+    if(questions.length>que_index){
+        count_que.innerText = que_index+1;
+        ShowQuestion(que_index);
+    }else{
+        console.log("Questions Complete");
+        quiz_box.classList.add("inactive");
+        result_box.classList.remove("inactive");
+        right_ans_r.innerText = right_answers;
+        wrong_ans_r.innerText = wrong_answers;
+        percentage.innerText = ((right_answers*100)/questions.length).toFixed(2)+"%";
+
+        
+    }
+
+    if(questions.length-1==que_index){
+        next_btn.innerText = "Finish";
+    }
+}
+
+function UserAnswer(answer){
+    let userAns = answer.innerText;
+    let correctAns = questions[que_index].answer;
+    var AllOptions2 = options_box.querySelectorAll(".option");
+
+    next_btn.classList.remove("inactive");
+    if(userAns == correctAns){
+        console.log("%c Right Answer","color:green");
+        answer.classList.add("correct");
+        answer.insertAdjacentHTML("beforeend",mark_check);
+        right_answers++;
+        score++;
+    }else{
+
+        console.log("%c Wrong Answer","color:red");
+        answer.classList.add("incorrect");
+        answer.insertAdjacentHTML("beforeend",mark_wrong);
+        wrong_answers++;
+
+        for(var i=0; i<AllOptions2.length; i++){
+            if(AllOptions2[i].innerText==correctAns){
+                AllOptions2[i].classList.add("correct");
+                AllOptions2[i].insertAdjacentHTML("beforeend",mark_check);
+            }
+        }
+    }
+
+
+    for(var j=0; j<AllOptions2.length; j++){
+        AllOptions2[j].classList.add("disabled");
+    }
+
+}
+
+again_quiz.onclick=()=>{
+    quiz_box.classList.remove("inactive");
+    result_box.classList.add("inactive");
+
+    reset();
+
+}
+
+exit.onclick=()=>{
+    start_btn.classList.remove("inactive");
+    result_box.classList.add("inactive");
+
+   reset();
+}
+
+function reset(){
+    que_index = 0;
+    right_answers = 0;
+    wrong_answers = 0;
+    score = 0;
+    next_btn.innerText = "Next Question";
+   count_que.innerText = que_index+1;
+   ShowQuestion(que_index);
+}
